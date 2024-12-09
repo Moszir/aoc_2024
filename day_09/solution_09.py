9ff86c8b8480ad4cf250015c9645c98442377a5f
Richárd Molnár-Szipai <richard.molnar-szipai@siemens.com>
2024-12-09 13:53:40 +0100

    day 9 counting without moving files around

--- day_09/solution_09.py
+++ day_09/solution_09.py
@@ -1,55 +1,48 @@
 disk_map = open('input.txt').read().strip()
 disk_size = sum(int(c) for c in disk_map)
-
 disk = [None] * disk_size
-position = 0
 spaces = [[] for i in range(10)]
+
+position = 0
 for i, c in enumerate(disk_map):
     size = int(c)
-    if i % 2 == 0:
-        # file
+    if i % 2 == 0:  # file
         file_id = i // 2
         for i in range(size):
             disk[position] = file_id
             position += 1
-    else:
-        # space
+    elif size > 0:  # space > 0
         spaces[size].append(position)
         position += size
 
 for i, s in enumerate(spaces):
     s.sort(reverse=True)
 
-disk2 = disk.copy()
-
+accu = 0
 space_index = next(i for i, value in enumerate(disk) if value is None)
-index = disk_size - 1
-while index > space_index:
-    if disk[index] is None:
-        index -= 1
-    else:
-        file_id = disk[index]
-        disk[index] = None
-        disk[space_index] = file_id
-        space_index += 1
-        while disk[space_index] is not None:
+for index in range(disk_size-1, -1, -1):
+    if disk[index] is not None:
+        if index > space_index:
+            accu += space_index * disk[index]
             space_index += 1
+            while disk[space_index] is not None:
+                space_index += 1
+        else:
+            accu += index * disk[index]
 
-def checksum(d):
-    return sum(i * value for i, value in enumerate(d) if value is not None)
+print(accu)
 
-print(checksum(disk))
-
+accu = 0
 index = disk_size - 1
 last_file_index = disk_size
 while index > 0:
     # find the next file
-    while index > 0 and (disk2[index] is None or disk2[index] >= last_file_index):
+    while index > 0 and (disk[index] is None or disk[index] >= last_file_index):
         index -= 1
-    file_id = disk2[index]
+    file_id = disk[index]
     last_file_index = file_id
     file_last = index
-    while disk2[index] == file_id:
+    while disk[index] == file_id:
         index -= 1
     file_first = index + 1
     file_size = file_last - file_first + 1
@@ -64,14 +57,15 @@ while index > 0:
 
     if space_index < file_first:
         # move the file to the space
-        for i in range(file_size):
-            disk2[space_index + i] = file_id
-            disk2[file_first + i] = None
-        spaces[space_size].pop()
+        accu += sum(i * file_id for i in range(space_index, space_index + file_size))
 
+        spaces[space_size].pop()
         remaining_space_size = space_size - file_size
         if remaining_space_size > 0:
             spaces[remaining_space_size].append(space_index + file_size)
             spaces[remaining_space_size].sort(reverse=True)
+    else:
+        # file stays in place
+        accu += sum(i * file_id for i in range(file_first, file_last + 1))
 
-print(checksum(disk2))
+print(accu)
