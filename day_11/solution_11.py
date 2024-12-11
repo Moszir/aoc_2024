90d205f618beafd64501dcd47182be5ff9e2c9cb
Richárd Molnár-Szipai <richard.molnar-szipai@siemens.com>
2024-12-11 09:41:58 +0100

    day 11

---
+++ day_11/solution_11.py
@@ -0,0 +1,17 @@
+from functools import cache
+
+numbers = list(map(int, open('input.txt').read().strip().split()))
+
+@cache
+def count(stone, steps):
+    if steps == 0:
+        return 1
+    if stone == 0:
+        return count(1, steps - 1)
+    if len(str(stone)) % 2 == 0:
+        s = str(stone)
+        left, right = int(s[:len(s) // 2]), int(s[len(s) // 2:])
+        return count(left, steps - 1) + count(right, steps - 1)
+    return count(2024 * stone, steps - 1)
+
+print(sum(count(stone, 75) for stone in numbers))
