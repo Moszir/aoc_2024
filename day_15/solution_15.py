8e496697b09c17d81e3f228a4162105473c482e9
Moszir <molnarszipai.richard@gmail.com>
2024-12-15 08:49:18 +0100

    day 15: part1 with part2's code rofl

--- day_15/solution_15.py
+++ day_15/solution_15.py
@@ -1,22 +1,27 @@
+import copy
 from collections import deque
 
 grid, moves = open('input.txt').read().strip().split('\n\n')
-grid = [[c for c in line] for line in grid.split('\n')]
-big_grid = [
-    [x
-     for c in line
-     for x in (('#', '#') if c == '#' else ('[', ']') if c == 'O' else ('.', '.') if c == '.' else ('@', '.'))]
-    for line in grid]
 moves = ''.join(moves.split('\n'))
 
+replace_with = {
+    '#': ('#', '#'),
+    'O': ('[', ']'),
+    '.': ('.', '.'),
+    '@': ('@', '.')
+}
+grid = [[x for c in line for x in replace_with[c]] for line in grid.split('\n')]
+original_robot = next((r, c) for r, row in enumerate(grid) for c, thing in enumerate(row) if thing == '@')
+grid[original_robot[0]][original_robot[1]] = '.'
 
-def solve(g):
-    r, c = next((r, c) for r, row in enumerate(g) for c, thing in enumerate(row) if thing == '@')
-    g[r][c] = '.'
+
+def solve(horizontal_step_factor=1):
+    g = copy.deepcopy(grid)
+    r, c = original_robot
 
     for move in moves:
         dr, dc = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}[move]
-        new_r, new_c = r + dr, c + dc
+        new_r, new_c = r + dr, c + dc * horizontal_step_factor
 
         if g[new_r][new_c] == '#':
             continue
@@ -25,8 +30,7 @@ def solve(g):
             r, c = new_r, new_c
             continue
 
-        # We have to push a block
-        to_be_processed = deque([(r, c)])
+        to_be_processed = deque([(r, c + j) for j in range(horizontal_step_factor)])
         visited = set()
         pushable = True
         while to_be_processed:
@@ -41,24 +45,22 @@ def solve(g):
                 pushable = False
                 break
             to_be_processed.extend(
-                [q] if gq == 'O' else
                 [q, (q[0], q[1] + 1)] if gq == '[' else
                 [q, (q[0], q[1] - 1)] if gq == ']' else
                 [])
         if not pushable:
             continue
 
-        # It is pushable, let's push it!
         push = [(p, g[p[0]][p[1]]) for p in visited]
         for p, _ in push:
             g[p[0]][p[1]] = '.'
         for p, q in push:
-            g[p[0] + dr][p[1] + dc] = q
+            g[p[0] + dr][p[1] + dc * horizontal_step_factor] = q
 
         r, c = new_r, new_c
 
-    return sum((100 * r + c) for r, row in enumerate(g) for c, t in enumerate(row) if t in ['[', 'O'])
+    return sum((100 * r + c // horizontal_step_factor) for r, row in enumerate(g) for c, t in enumerate(row) if t == '[')
 
 
-print(solve(grid))  # 1463512
-print(solve(big_grid))  # 1486520
+print(solve(2))
+print(solve())
