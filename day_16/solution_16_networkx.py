4a00c50ef01ad4fcfa423a81e56e6d78d23895d2
Moszir <molnarszipai.richard@gmail.com>
2024-12-23 09:07:40 +0100

    day 16 with networkx

---
+++ day_16/solution_16_networkx.py
@@ -0,0 +1,46 @@
+""" Advent of Code 2024 Day 16
+
+https://adventofcode.com/2024/day/16
+"""
+import networkx as nx
+
+grid = open('input.txt').read().strip().split('\n')
+
+starting_position = None
+target_positions = None
+G = nx.DiGraph()
+for r, row in enumerate(grid):
+    for c, cell in enumerate(row):
+        if cell != '#':
+            # Turning is expensive
+            G.add_edge((r, c, 1, 0), (r, c, 0, -1), weight=1000)
+            G.add_edge((r, c, 1, 0), (r, c, 0, 1), weight=1000)
+            G.add_edge((r, c, -1, 0), (r, c, 0, -1), weight=1000)
+            G.add_edge((r, c, -1, 0), (r, c, 0, 1), weight=1000)
+            G.add_edge((r, c, 0, 1), (r, c, -1, 0), weight=1000)
+            G.add_edge((r, c, 0, 1), (r, c, 1, 0), weight=1000)
+            G.add_edge((r, c, 0, -1), (r, c, -1, 0), weight=1000)
+            G.add_edge((r, c, 0, -1), (r, c, 1, 0), weight=1000)
+
+            for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                if grid[r + dr][c + dc] != '#':
+                    # Moving to the cell, keeping the direction
+                    G.add_edge((r, c, dr, dc), (r + dr, c + dc, dr, dc), weight=1)
+
+            if cell == 'S':
+                starting_position = (r, c, 0, 1)  # problem statement says we start facing east
+            if cell == 'E':
+                assert target_positions is None
+                target_positions = [(r, c, 1, 0), (r, c, -1, 0), (r, c, 0, 1), (r, c, 0, -1)]
+
+assert starting_position is not None
+assert target_positions is not None
+
+costs = {tp: nx.shortest_path_length(G, source=starting_position, target=tp, weight='weight') for tp in target_positions}
+min_cost = min(costs.values())
+print(min_cost)
+print(len(set((r, c)
+              for tp, cost in costs.items()
+              if cost == min_cost
+              for sp in nx.all_shortest_paths(G, source=starting_position, target=tp, weight='weight')
+              for r, c, _, _ in sp)))
