b83e596eafbca271dfcf0704b8c11064e3adc614
Moszir <molnarszipai.richard@gmail.com>
2024-12-23 09:09:21 +0100

    day 16 with networkx

--- day_16/solution_16_networkx.py
+++ day_16/solution_16_networkx.py
@@ -12,20 +12,11 @@ G = nx.DiGraph()
 for r, row in enumerate(grid):
     for c, cell in enumerate(row):
         if cell != '#':
-            # Turning is expensive
-            G.add_edge((r, c, 1, 0), (r, c, 0, -1), weight=1000)
-            G.add_edge((r, c, 1, 0), (r, c, 0, 1), weight=1000)
-            G.add_edge((r, c, -1, 0), (r, c, 0, -1), weight=1000)
-            G.add_edge((r, c, -1, 0), (r, c, 0, 1), weight=1000)
-            G.add_edge((r, c, 0, 1), (r, c, -1, 0), weight=1000)
-            G.add_edge((r, c, 0, 1), (r, c, 1, 0), weight=1000)
-            G.add_edge((r, c, 0, -1), (r, c, -1, 0), weight=1000)
-            G.add_edge((r, c, 0, -1), (r, c, 1, 0), weight=1000)
-
             for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                G.add_edge((r, c, dr, dc), (r, c, -dc, dr), weight=1000)  # turns
+                G.add_edge((r, c, dr, dc), (r, c, dc, -dr), weight=1000)
                 if grid[r + dr][c + dc] != '#':
-                    # Moving to the cell, keeping the direction
-                    G.add_edge((r, c, dr, dc), (r + dr, c + dc, dr, dc), weight=1)
+                    G.add_edge((r, c, dr, dc), (r + dr, c + dc, dr, dc), weight=1)  # forward
 
             if cell == 'S':
                 starting_position = (r, c, 0, 1)  # problem statement says we start facing east
@@ -40,7 +31,6 @@ costs = {tp: nx.shortest_path_length(G, source=starting_position, target=tp, wei
 min_cost = min(costs.values())
 print(min_cost)
 print(len(set((r, c)
-              for tp, cost in costs.items()
-              if cost == min_cost
+              for tp, cost in costs.items() if cost == min_cost
               for sp in nx.all_shortest_paths(G, source=starting_position, target=tp, weight='weight')
               for r, c, _, _ in sp)))
